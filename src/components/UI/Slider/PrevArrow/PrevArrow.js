57411205b073f9a2c76df80665280c8fa563a9e5
Dragan Trivic <dragantrivic@Dragans-iMac.local>
2020-10-25 15:24:56 +0100

    Create custom arrow components for category slider

---
+++ src/components/UI/Slider/PrevArrow/PrevArrow.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const PrevArrow = (props) => {
+    const { onClick } = props;
+
+    const style = {
+        display: 'block',
+        position: 'absolute',
+        left: '-12px',
+        fontSize: '0',
+        lineHeight: '0',
+        top: '50%',
+        width: '48px',
+        height: '48px',
+        borderRadius: '24px',
+        boxShadow: 'rgba(0, 0, 0, 0.14) 0px 1px 1px 1px',
+        background: '#fff',
+        zIndex: '1',
+        WebkitTransform: 'translate(0,-50%)',
+        MsTransform: 'translate(0,-50%)',
+        transform: 'translate(0,-50%)',
+        cursor: 'pointer',
+        border: 'none',
+        outline: '0'
+    }
+
+    return (
+        <div
+            style={style}
+            onClick={onClick}>
+                <svg className="w-6 h-6 mx-auto mt-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+                </svg>
+        </div>
+    );
+}
+
+export default PrevArrow;
